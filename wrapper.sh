b4e1c9ecd935490df105c27928748e13f8bb0cd8
LoreMagnani <lorenzo220901@gmail.com>
2023-12-19 15:39:58 +0100

    ciao

---
+++ wrapper.sh
@@ -0,0 +1,11 @@
+mkdir tmp
+cd tmp
+cp ../$1_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz ./
+tar -avxf $1_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz
+date
+echo $1
+echo this is the $2-th job
+./runcmsgrid.sh 1000 $2 4
+date
+mv *lhe .. && echo 'Generate events successfully!'
+rm -rf tmp
